d35791d32fa191dd885b7459abcc414a18329f65
wutsi <herve.tchepannou@wutsi.com>
2021-11-30 21:39:12 -0500

    validate account

--- src/main/kotlin/com/wutsi/application/cash/endpoint/send/command/SendAmountCommand.kt
+++ src/main/kotlin/com/wutsi/application/cash/endpoint/send/command/SendAmountCommand.kt
@@ -2,6 +2,7 @@ package com.wutsi.application.cash.endpoint.send.command
 
 import com.wutsi.application.cash.endpoint.AbstractCommand
 import com.wutsi.application.cash.endpoint.send.dto.SendAmountRequest
+import com.wutsi.application.cash.service.SecurityManager
 import com.wutsi.application.cash.service.URLBuilder
 import com.wutsi.flutter.sdui.Action
 import com.wutsi.flutter.sdui.Dialog
@@ -9,6 +10,9 @@ import com.wutsi.flutter.sdui.enums.ActionType.Prompt
 import com.wutsi.flutter.sdui.enums.ActionType.Route
 import com.wutsi.flutter.sdui.enums.DialogType.Error
 import com.wutsi.platform.core.logging.KVLogger
+import com.wutsi.platform.payment.WutsiPaymentApi
+import com.wutsi.platform.payment.core.Money
+import feign.FeignException
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,12 +24,26 @@ import javax.validation.Valid
 class SendAmountCommand(
     private val logger: KVLogger,
     private val urlBuilder: URLBuilder,
+    private val paymentApi: WutsiPaymentApi,
+    private val securityManager: SecurityManager
 ) : AbstractCommand() {
     @PostMapping
     fun index(@RequestBody @Valid request: SendAmountRequest): Action {
         logger.add("amount", request.amount)
 
         // Validate
+        val action = validate(request)
+        if (action != null)
+            return action
+
+        // Goto next page
+        return Action(
+            type = Route,
+            url = urlBuilder.build("send/recipient?amount=${request.amount}")
+        )
+    }
+
+    private fun validate(request: SendAmountRequest): Action? {
         if (request.amount == 0.0)
             return Action(
                 type = Prompt,
@@ -35,10 +53,25 @@ class SendAmountCommand(
                 )
             )
 
-        // Goto next page
-        return Action(
-            type = Route,
-            url = urlBuilder.build("send/recipient?amount=${request.amount}")
-        )
+        val balance = getBalance()
+        if (request.amount > balance.value)
+            return Action(
+                type = Prompt,
+                prompt = Dialog(
+                    type = Error,
+                    message = getText("prompt.error.transaction-failed.NOT_ENOUGH_FUNDS")
+                )
+            )
+
+        return null
+    }
+
+    private fun getBalance(): Money {
+        try {
+            val balance = paymentApi.getBalance(securityManager.currentUserId()).balance
+            return Money(balance.amount, balance.currency)
+        } catch (ex: FeignException.NotFound) {
+            return Money(0.0, "")
+        }
     }
 }
--- src/test/kotlin/com/wutsi/application/cash/endpoint/send/command/SendAmountCommandTest.kt
+++ src/test/kotlin/com/wutsi/application/cash/endpoint/send/command/SendAmountCommandTest.kt
@@ -1,15 +1,25 @@
 package com.wutsi.application.cash.endpoint.send.command
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.application.cash.endpoint.AbstractEndpointTest
 import com.wutsi.application.cash.endpoint.send.dto.SendAmountRequest
 import com.wutsi.flutter.sdui.Action
+import com.wutsi.flutter.sdui.enums.ActionType
 import com.wutsi.flutter.sdui.enums.ActionType.Route
+import com.wutsi.flutter.sdui.enums.DialogType
 import com.wutsi.platform.payment.WutsiPaymentApi
+import com.wutsi.platform.payment.dto.Balance
+import com.wutsi.platform.payment.dto.GetBalanceResponse
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.context.MessageSource
+import java.util.Locale
 import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,6 +29,9 @@ internal class SendAmountCommandTest : AbstractEndpointTest() {
 
     private lateinit var url: String
 
+    @Autowired
+    private lateinit var messages: MessageSource
+
     @MockBean
     private lateinit var paymentApi: WutsiPaymentApi
 
@@ -27,6 +40,8 @@ internal class SendAmountCommandTest : AbstractEndpointTest() {
         super.setUp()
 
         url = "http://localhost:$port/commands/send/amount"
+
+        doReturn(GetBalanceResponse(Balance(amount = 10000.0))).whenever(paymentApi).getBalance(any())
     }
 
     @Test
@@ -43,5 +58,45 @@ internal class SendAmountCommandTest : AbstractEndpointTest() {
         val action = response.body
         assertEquals(Route, action.type)
         assertEquals("http://localhost:0/send/recipient?amount=1000.0", action.url)
+    }
+
+    @Test
+    fun zero() {
+        // WHEN
+        val request = SendAmountRequest(
+            amount = 0.0
+        )
+        val response = rest.postForEntity(url, request, Action::class.java)
+
+        // THEN
+        assertEquals(200, response.statusCodeValue)
+
+        val action = response.body
+        assertEquals(ActionType.Prompt, action.type)
+        assertEquals(DialogType.Error, action.prompt?.type)
+        assertEquals(
+            messages.getMessage("prompt.error.amount-required", emptyArray(), Locale.ENGLISH),
+            action.prompt?.message
+        )
+    }
+
+    @Test
+    fun notEnoughFunds() {
+        // WHEN
+        val request = SendAmountRequest(
+            amount = 1000000.0
+        )
+        val response = rest.postForEntity(url, request, Action::class.java)
+
+        // THEN
+        assertEquals(200, response.statusCodeValue)
+
+        val action = response.body
+        assertEquals(ActionType.Prompt, action.type)
+        assertEquals(DialogType.Error, action.prompt?.type)
+        assertEquals(
+            messages.getMessage("prompt.error.transaction-failed.NOT_ENOUGH_FUNDS", emptyArray(), Locale.ENGLISH),
+            action.prompt?.message
+        )
     }
 }
