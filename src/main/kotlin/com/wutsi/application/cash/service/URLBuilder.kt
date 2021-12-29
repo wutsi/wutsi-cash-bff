19765700aa630d228e9a90868160463da668ed9d
wutsi <herve.tchepannou@wutsi.com>
2021-12-28 22:26:49 -0500

    fix url builder

--- src/main/kotlin/com/wutsi/application/cash/service/URLBuilder.kt
+++ src/main/kotlin/com/wutsi/application/cash/service/URLBuilder.kt
@@ -9,5 +9,17 @@ class URLBuilder(
 ) {
     fun build(path: String) = build(serverUrl, path)
 
-    fun build(baseUrl: String, path: String) = "$baseUrl/$path"
+    fun build(prefix: String, path: String): String {
+        val xprefix = if (prefix.endsWith("/"))
+            prefix.substring(0, prefix.length - 1)
+        else
+            prefix
+
+        val xpath = if (path.startsWith("/"))
+            path.substring(1)
+        else
+            path
+
+        return "$xprefix/$xpath"
+    }
 }
