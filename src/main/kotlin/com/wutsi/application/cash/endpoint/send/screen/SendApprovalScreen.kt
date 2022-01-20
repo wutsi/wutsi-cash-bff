6c42400f21d40b87176776b0d095146a4c27515d
wutsi <herve.tchepannou@wutsi.com>
2022-01-20 08:39:53 -0500

    change approval screen

--- src/main/kotlin/com/wutsi/application/cash/endpoint/send/screen/SendApprovalScreen.kt
+++ src/main/kotlin/com/wutsi/application/cash/endpoint/send/screen/SendApprovalScreen.kt
@@ -7,22 +7,11 @@ import com.wutsi.application.shared.service.CategoryService
 import com.wutsi.application.shared.service.TenantProvider
 import com.wutsi.application.shared.service.TogglesProvider
 import com.wutsi.application.shared.service.URLBuilder
-import com.wutsi.application.shared.ui.ProfileCard
-import com.wutsi.application.shared.ui.ProfileCardType
-import com.wutsi.flutter.sdui.Action
 import com.wutsi.flutter.sdui.AppBar
-import com.wutsi.flutter.sdui.Button
-import com.wutsi.flutter.sdui.Column
 import com.wutsi.flutter.sdui.Container
-import com.wutsi.flutter.sdui.Divider
 import com.wutsi.flutter.sdui.IconButton
-import com.wutsi.flutter.sdui.MoneyText
 import com.wutsi.flutter.sdui.Screen
-import com.wutsi.flutter.sdui.Text
 import com.wutsi.flutter.sdui.Widget
-import com.wutsi.flutter.sdui.enums.ActionType.Command
-import com.wutsi.flutter.sdui.enums.Alignment
-import com.wutsi.flutter.sdui.enums.ButtonType
 import com.wutsi.platform.account.WutsiAccountApi
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -61,65 +50,66 @@ class SendApprovalScreen(
                 ),
                 title = getText("page.send-approval.app-bar.title"),
             ),
-            child = Column(
-                children = listOf(
-                    Container(
-                        padding = 10.0,
-                        alignment = Alignment.Center,
-                        child = Text(
-                            caption = getText("page.send-approval.message"),
-                            size = Theme.TEXT_SIZE_LARGE,
-                        )
-                    ),
-                    MoneyText(
-                        value = tx.net,
-                        currency = tenant.currencySymbol,
-                        numberFormat = tenant.numberFormat,
-                        color = Theme.COLOR_PRIMARY
-                    ),
-                    Container(
-                        alignment = Alignment.Center,
-                        child = Text(
-                            getText("page.send-approval.fees", arrayOf(fmt.format(tx.fees))),
-                            bold = true,
-                            size = Theme.TEXT_SIZE_LARGE
-                        )
-                    ),
-                    Divider(color = Theme.COLOR_DIVIDER),
-                    Container(
-                        padding = 10.0,
-                        alignment = Alignment.Center,
-                        child = Text(
-                            caption = getText("page.send-approval.from"),
-                            size = Theme.TEXT_SIZE_LARGE,
-                        )
-                    ),
-                    ProfileCard(
-                        account = sender,
-                        phoneNumber = null,
-                        categoryService = categoryService,
-                        togglesProvider = togglesProvider,
-                        showWebsite = false,
-                        type = ProfileCardType.Summary
-                    ),
-                    Divider(color = Theme.COLOR_DIVIDER),
-                    Container(
-                        padding = 10.0,
-                        child = Button(
-                            caption = getText("page.send-approval.button.submit", arrayOf(fmt.format(tx.amount))),
-                            action = Action(
-                                type = Command,
-                                url = urlBuilder.build("commands/send/approve?transaction-id=$transactionId")
-                            )
-                        )
-                    ),
-                    Button(
-                        type = ButtonType.Text,
-                        caption = getText("page.send-approval.button.cancel"),
-                        action = gotoHome()
-                    )
-                )
-            ),
+            child = Container()
+//            child = Column(
+//                children = listOf(
+//                    Container(
+//                        padding = 10.0,
+//                        alignment = Alignment.Center,
+//                        child = Text(
+//                            caption = getText("page.send-approval.message"),
+//                            size = Theme.TEXT_SIZE_LARGE,
+//                        )
+//                    ),
+//                    MoneyText(
+//                        value = tx.net,
+//                        currency = tenant.currencySymbol,
+//                        numberFormat = tenant.numberFormat,
+//                        color = Theme.COLOR_PRIMARY
+//                    ),
+//                    Container(
+//                        alignment = Alignment.Center,
+//                        child = Text(
+//                            getText("page.send-approval.fees", arrayOf(fmt.format(tx.fees))),
+//                            bold = true,
+//                            size = Theme.TEXT_SIZE_LARGE
+//                        )
+//                    ),
+//                    Divider(color = Theme.COLOR_DIVIDER),
+//                    Container(
+//                        padding = 10.0,
+//                        alignment = Alignment.Center,
+//                        child = Text(
+//                            caption = getText("page.send-approval.from"),
+//                            size = Theme.TEXT_SIZE_LARGE,
+//                        )
+//                    ),
+//                    ProfileCard(
+//                        account = sender,
+//                        phoneNumber = null,
+//                        categoryService = categoryService,
+//                        togglesProvider = togglesProvider,
+//                        showWebsite = false,
+//                        type = ProfileCardType.Summary
+//                    ),
+//                    Divider(color = Theme.COLOR_DIVIDER),
+//                    Container(
+//                        padding = 10.0,
+//                        child = Button(
+//                            caption = getText("page.send-approval.button.submit", arrayOf(fmt.format(tx.amount))),
+//                            action = Action(
+//                                type = Command,
+//                                url = urlBuilder.build("commands/send/approve?transaction-id=$transactionId")
+//                            )
+//                        )
+//                    ),
+//                    Button(
+//                        type = ButtonType.Text,
+//                        caption = getText("page.send-approval.button.cancel"),
+//                        action = gotoHome()
+//                    )
+//                )
+//            ),
         ).toWidget()
     }
 }
